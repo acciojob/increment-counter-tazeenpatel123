d625b315156d5583762b6cc3ec5c057cffcca613
tazeenpatel123 <150174275+tazeenpatel123@users.noreply.github.com>
2023-12-10 19:45:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const counter=document.getElementById("counter");
+const incrementBtn=document.getElementById("incrementBtn");
+let cnt=0;
+incrementBtn.addeventListener("click",() =>{
+	alert(`current counter value:${cnt}`);
+	cnt++;
+	counter.textContent=cnt;
+});
